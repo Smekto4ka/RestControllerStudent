53ec8e79fd0efee81e8b998cb1ca5bb8150730fa
Smekto4ka <barsalekcandr@mail.ru>
2020-11-22 17:22:13 +0300

    new Stud v2

--- StudentAngular/src/app/post-student/post-student.component.ts
+++ StudentAngular/src/app/post-student/post-student.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {StudentService} from '../shared/service/student.service';
+import {Student} from '../model/Student';
 
 @Component({
   selector: 'app-post-student',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostStudentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private studentService: StudentService) {
+  }
+
+  lastName = '';
+  firstName = '';
+  years = 0;
 
   ngOnInit(): void {
   }
 
+  postStudent() {
+    this.studentService.postStudent(new Student(0, this.firstName, this.lastName, this.years, []));
+  }
 }
