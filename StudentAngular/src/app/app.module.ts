d1760a1f09823f0e195bbea30b343a0e6b4c7c61
Smekto4ka <barsalekcandr@mail.ru>
2020-11-22 17:39:34 +0300

    router

--- StudentAngular/src/app/app.module.ts
+++ StudentAngular/src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { HttpClientModule} from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+import {RouterModule , Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { StudentComponent } from './student/student.component';
 import {FormsModule} from '@angular/forms';
 import { PostStudentComponent } from './post-student/post-student.component';
 
+const appRoutes: Routes = [
+  {path : 'listStudent' , component: StudentComponent},
+  {path: 'newStudent' , component: PostStudentComponent}
+];
 
 @NgModule({
   declarations: [
@@ -17,7 +21,7 @@ import { PostStudentComponent } from './post-student/post-student.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
     HttpClientModule,
     FormsModule
   ],
