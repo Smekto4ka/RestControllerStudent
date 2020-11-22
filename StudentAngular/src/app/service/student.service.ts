973e6150719afc0fff8e54e2eddcb1be79f28d2d
Smekto4ka <barsalekcandr@mail.ru>
2020-11-22 16:30:50 +0300

    put

--- StudentAngular/src/app/service/student.service.ts
+++ StudentAngular/src/app/service/student.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Student} from '../model/Student';
+import {catchError} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class StudentService {
@@ -14,7 +15,7 @@ export class StudentService {
   }
 
   updateStudent(student: Student): Observable<any> {
-    return this.http.put('http://localhost:8180/restStudent', student);
+    return this.http.put('http://localhost:8180/restStudent', student).subscribe();
   }
 
 }
